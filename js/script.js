1f416de7cdb0605522cfebaa6954a32c189f9e14
neenael <109112345+neenael@users.noreply.github.com>
2023-03-24 23:26:40 +0300

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -11,3 +11,43 @@ const checkbox = document.getElementById("dark-mode-checkbox");
   checkbox.addEventListener('click', function(){
     document.body.classList.toggle('dark-theme');
   })
+
+  document.addEventListener("DOMContentLoaded", function() {
+    let acc = new Accordion('.accordion__list', {
+    duration: 700,
+    elementClass: 'accordion__item',
+    triggerClass: 'accordion__top',
+    panelClass: 'accordion__bottom',
+    showMultiple: false,
+  
+      collapse: true
+  });
+  })
+
+
+  document
+  .querySelector(".header__burger_btn")
+  .addEventListener("click", function () {
+
+    document.body.classList.toggle("stop-scroll");
+    document.querySelector(".nav__menu").classList.toggle("nav--active");
+    document.querySelector(".header__burger_btn").classList.toggle("active");
+  });
+
+document.querySelectorAll(".header-link").forEach(function(elem){
+  elem.addEventListener("click", function () {
+    document.body.classList.remove("stop-scroll");
+    document.querySelector(".nav__menu").classList.remove("nav--active");
+    document.querySelector(".header__burger_btn").classList.remove("active");
+  });
+})
+
+tippy('#binding_i', {
+  content: 'Обложка + пружина + картон',
+});
+tippy('#wide_print_i', {
+  content: 'Cтоимость Ч/Б и цветной печати одинакова',
+});
+
+
+
